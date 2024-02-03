3d15887dcb7e4c25cea6f49831e180ed33c98aee
Bastian <badmaxyt@gmail.com>
2024-02-03 18:52:27 +0100

    feat: :building_construction: add new base components

---
+++ src/app/compte-formulaire/compte-formulaire.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compte-formulaire',
+  standalone: true,
+  imports: [],
+  templateUrl: './compte-formulaire.component.html',
+  styleUrl: './compte-formulaire.component.css'
+})
+export class CompteFormulaireComponent {
+
+}
---
+++ src/app/recapitulatif/recapitulatif.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recapitulatif',
+  standalone: true,
+  imports: [],
+  templateUrl: './recapitulatif.component.html',
+  styleUrl: './recapitulatif.component.css'
+})
+export class RecapitulatifComponent {
+
+}
