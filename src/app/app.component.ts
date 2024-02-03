d47925ee5f17c7352d7dc1c556272062a6ed79a4
Bastian <badmaxyt@gmail.com>
2024-02-03 19:26:47 +0100

    feat: :lipstick: update UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+/* Components */
+import { CompteFormulaireComponent } from './compte-formulaire/compte-formulaire.component';
+import { FooterComponent } from './footer/footer.component';
+import { RecapitulatifComponent } from './recapitulatif/recapitulatif.component';
+import { TetiereComponent } from './tetiere/tetiere.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CompteFormulaireComponent, FooterComponent, RecapitulatifComponent, RouterOutlet, TetiereComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
