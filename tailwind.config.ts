d62d333b657cb2b9ad846a40820f8e246f181be8
Bastian <badmaxyt@gmail.com>
2024-02-08 00:24:44 +0100

    feat(global_styles): :lipstick: add inter and merienda in global css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ export default {
       colors: {
         "primary": "#D1392A",
         "secondary": "#9D6F6F",
+        "accent": "#F7F7F7",
+      },
+      fontFamily: {
+        "inter": ["Inter", "sans-serif"],
+        'merienda': ['Merienda', 'cursive'],
       },
     },
   },
