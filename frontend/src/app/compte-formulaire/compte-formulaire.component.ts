438ebfc82543f231f850821c75e6718fe9aebc4d
Bastian <badmaxyt@gmail.com>
2024-02-12 19:12:47 +0100

    feat(compte_formulaire): :sparkles: add form with validator

--- frontend/src/app/compte-formulaire/compte-formulaire.component.ts
+++ frontend/src/app/compte-formulaire/compte-formulaire.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-compte-formulaire',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './compte-formulaire.component.html',
   styleUrl: './compte-formulaire.component.css'
 })
-export class CompteFormulaireComponent {
+export class CompteFormulaireComponent implements OnInit {
+  inscriptionForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.inscriptionForm = this.fb.group({
+      civilite: ['', Validators.required],
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      adresse: ['', Validators.required],
+      ville: ['', Validators.required],
+      code_postal: ['', [Validators.required, Validators.pattern('^[0-9]{5}$')]],
+      telephone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      pays: ['', Validators.required],
+      login: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      motdepasse: ['', Validators.required],
+      mdp_conf: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.inscriptionForm.valid) {
+      console.log('Formulaire valide');
+    }
+  }
 }
