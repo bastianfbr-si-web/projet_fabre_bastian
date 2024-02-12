985b1d2e7d9341a5f35d9fcf787e0b99d02a6e44
Bastian <badmaxyt@gmail.com>
2024-02-12 22:46:37 +0100

    feat(recapitulatif): :sparkles: add recapitulatif logic

--- frontend/src/app/recapitulatif/recapitulatif.component.ts
+++ frontend/src/app/recapitulatif/recapitulatif.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormulaireService } from '../services/formulaire.service';
+import { InscriptionForm } from '../interfaces/inscriptionForm';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recapitulatif',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recapitulatif.component.html',
-  styleUrl: './recapitulatif.component.css'
+  styleUrls: ['./recapitulatif.component.css']
 })
-export class RecapitulatifComponent {
+export class RecapitulatifComponent implements OnInit {
+  formData: InscriptionForm = new InscriptionForm('','','','','','','','','','','','')
 
+  constructor(private formulaireService: FormulaireService) { }
+
+  ngOnInit(): void {
+    this.formulaireService.getFormData().subscribe(data => {
+      this.formData = data;
+    });
+  }
 }
