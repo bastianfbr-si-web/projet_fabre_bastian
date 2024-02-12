6811f75a7c6e95dce445d4b04bd4e9f354cb25c0
Bastian <badmaxyt@gmail.com>
2024-02-12 22:47:11 +0100

    feat(inscriptionForm): :bricks: add inscriptionForm model

---
+++ frontend/src/app/interfaces/inscriptionForm.ts
@@ -0,0 +1,16 @@
+export class InscriptionForm {
+  constructor(
+    public civilite: string,
+    public nom: string,
+    public prenom: string,
+    public adresse: string,
+    public ville: string,
+    public code_postal: string,
+    public telephone: string,
+    public pays: string,
+    public login: string,
+    public email: string,
+    public motdepasse: string,
+    public mdp_conf: string
+  ) {  }
+}
