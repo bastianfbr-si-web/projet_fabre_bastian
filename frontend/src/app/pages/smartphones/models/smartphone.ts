c21779ac3aa24782dd0e59b6c1d07fe310553fb7
Bastian <badmaxyt@gmail.com>
2024-02-29 16:48:08 +0100

    feat(smartphone): :bricks: add smartphone model

---
+++ frontend/src/app/pages/smartphones/models/smartphone.ts
@@ -0,0 +1,11 @@
+export class Smartphone {
+  constructor(
+    public id: number,
+    public name: string,
+    public price: number,
+    public brand: string,
+    public color: string,
+    public description: string,
+    public image: string
+  ) {}
+}
