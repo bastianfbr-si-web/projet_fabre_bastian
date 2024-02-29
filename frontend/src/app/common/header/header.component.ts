cb80284355bb24dc1f09cf0d862fdeebd53e57ba
Bastian <badmaxyt@gmail.com>
2024-02-29 22:52:34 +0100

    refactor(search): :recycle: extract searchBar to new component

--- frontend/src/app/common/header/header.component.ts
+++ frontend/src/app/common/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SearchComponent } from "./search/search.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+    imports: [RouterLink, SearchComponent]
 })
 export class HeaderComponent {
 
