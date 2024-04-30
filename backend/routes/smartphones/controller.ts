1d615e6264c2b532060b4fd3f510895bb8e15acd
Bastian <badmaxyt@gmail.com>
2024-04-30 12:06:25 +0200

    feat(backend): :sparkles: add queries in /smartphones route

--- backend/routes/smartphones/controller.ts
+++ backend/routes/smartphones/controller.ts
@@ -2,7 +2,8 @@ import smartphonesService from './services';
 import { Request, Response } from 'express';
 
 function getAll(req: Request, res: Response) {
-    const smartphones = smartphonesService.getAll();
+    const { prixMin, prixMax, nom, marque } = req.query as { prixMin: string, prixMax: string, nom: string, marque: string };
+    const smartphones = smartphonesService.getAll(prixMin, prixMax, nom, marque);
     res.send(smartphones);
 }
 
--- backend/routes/smartphones/services.ts
+++ backend/routes/smartphones/services.ts
@@ -1,7 +1,21 @@
 import smartphones from '../../mocks/smartphones.json';
 
-function getAll() {
-    return smartphones;
+function getAll(priceMin: string, priceMax: string, name: string, brand: string) {
+    return smartphones.filter(smartphone => {
+        if (priceMin && smartphone.prix < parseInt(priceMin)) {
+            return false;
+        }
+        if (priceMax && smartphone.prix > parseInt(priceMax)) {
+            return false;
+        }
+        if (name && !smartphone.nom.toLowerCase().includes(name.toLowerCase())) {
+            return false;
+        }
+        if (brand && !smartphone.marque.toLowerCase().includes(brand.toLowerCase())) {
+            return false;
+        }
+        return true;
+    });
 }
 
 export default { getAll };
