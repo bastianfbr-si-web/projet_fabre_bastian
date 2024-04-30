fa5352660a630a5de88f5b913789aa9c26e336cc
Bastian <badmaxyt@gmail.com>
2024-04-30 08:48:04 +0200

    fix(backend): :bug: finish jwt verify

--- backend/middlewares/jwt.ts
+++ backend/middlewares/jwt.ts
@@ -1,18 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-const TOKEN = process.env.TOKEN;
+const secretToken = process.env.TOKEN;
 
 function validateJWT(req: Request, res: Response, next: NextFunction) {
     const token = req.headers.authorization?.split(" ")[1];
+    let jwtPayload;
 
     if (!token) {
         return res.status(401).send("Accès refusé. Aucun token fourni.");
     }
 
     try {
-        jwt.verify(token, TOKEN!);
+        jwtPayload = jwt.verify(token, secretToken!,
+        {
+          complete: true,
+          algorithms: ['HS256'],
+          clockTolerance: 0,
+          ignoreExpiration: false,
+          ignoreNotBefore: false
+        });
         next();
+        req.body.jwtPayload = jwtPayload;
     } catch (error) {
         return res.status(401).send("Accès refusé. Token invalide.");
     }
